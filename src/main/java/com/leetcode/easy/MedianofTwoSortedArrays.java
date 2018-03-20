a51d090e548e7e5c043ad1a0ea813e69cd0610c3
kekewang <wangke59@hotmail.com>
2018-03-20 16:50:10 +0800

    20180319 - Add solution: Median of Two Sorted Arrays.

--- src/main/java/com/leetcode/easy/MedianofTwoSortedArrays.java
+++ src/main/java/com/leetcode/easy/MedianofTwoSortedArrays.java
@@ -29,6 +29,9 @@ public class MedianofTwoSortedArrays {
     }
 
     public double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
+        printArray("A:", A, aStart, k);
+        printArray("B:", B, bStart, k);
+        System.out.println();
         if (aStart > A.length - 1) return B[bStart + k - 1];
         if (bStart > B.length - 1) return A[aStart + k - 1];
         if (k == 1) return Math.min(A[aStart], B[bStart]);
@@ -36,16 +39,25 @@ public class MedianofTwoSortedArrays {
         int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
         if (aStart + k / 2 - 1 < A.length) aMid = A[aStart + k / 2 - 1];
         if (bStart + k / 2 - 1 < B.length) bMid = B[bStart + k / 2 - 1];
-
+        System.out.print("aMid=" + aMid + ", bMid=" + bMid);
+        System.out.println();
+        System.out.println("=========================================");
         if (aMid < bMid)
             return getkth(A, aStart + k / 2, B, bStart, k - k / 2);// Check: aRight + bLeft
         else
             return getkth(A, aStart, B, bStart + k / 2, k - k / 2);// Check: bRight + aLeft
     }
 
+    private void printArray(String preText, int[] A, int start, int k) {
+        System.out.print(preText);
+        for (int i = 0; i < k; i++) {
+            System.out.print(A[start + i] + ", ");
+        }
+    }
+
     public static void main(String[] args) {
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2};
+        int[] nums1 = {1, 3, 5, 7, 9, 11};
+        int[] nums2 = {2, 4, 6, 8, 10, 12};
         MedianofTwoSortedArrays m = new MedianofTwoSortedArrays();
         double result = m.findMedianSortedArrays(nums1, nums2);
         System.out.println(result);

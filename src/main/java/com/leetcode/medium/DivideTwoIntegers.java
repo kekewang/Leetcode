5daf2d863113e3139d9ef308ebfeb47b94030661
kekewang <wangke59@hotmail.com>
2019-03-10 22:44:32 +0800

    20190310 - Divide Two Integers

--- src/main/java/com/leetcode/medium/DivideTwoIntegers.java
+++ src/main/java/com/leetcode/medium/DivideTwoIntegers.java
@@ -1,13 +1,54 @@
 package com.leetcode.medium;
 
+/**
+ * Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+ * <p>
+ * Return the quotient after dividing dividend by divisor.
+ * <p>
+ * The integer division should truncate toward zero.
+ */
 public class DivideTwoIntegers {
 
     public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
 
-        return 0;
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        int result = 0;
+        long x = Math.abs((long) dividend);
+        long y = Math.abs((long) divisor);
+
+        while (x >= y) {
+            x -= y;
+            result++;
+        }
+        return result * sign;
+    }
+
+    public int divide1(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        int result = 0;
+        long x = Math.abs((long) dividend);
+        long y = Math.abs((long) divisor);
+
+        while (x >= y) {
+            int shift = 1;
+
+            while (x >= (y << shift)) {
+                shift++;
+            }
+            x -= y << (shift - 1);
+            result += 1 << (shift - 1);
+        }
+        return result * sign;
     }
 
     public static void main(String[] args) {
 
+        System.out.println(new DivideTwoIntegers().divide(10, 3));
+        System.out.println(new DivideTwoIntegers().divide1(92, 6));
     }
 }

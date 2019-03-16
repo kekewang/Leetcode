b1355bdb1f9236f49b6b5fc0e5de86dcca729533
kekewang <wangke59@hotmail.com>
2019-03-16 21:40:15 +0800

    20190316 - Valid Parentheses.

---
+++ src/main/java/com/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,21 @@
+package com.leetcode.easy;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char temp : s.toCharArray()) {
+            if (temp == '(' || temp == '{' || temp == '[')
+                stack.push(temp);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                char last = stack.pop();
+                if ((temp == ')' && last != '(') || (temp == '}' && last != '{') || (temp == ']' && last != '['))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
